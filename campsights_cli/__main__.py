e6883752dacdf0a784a96c0d9729afa5fca3b34e
Matt Carnovale <matthew.carnovale@gmail.com>
2018-08-13 17:35:17 -0700

    Implements option to get a list of campgrounds by partial address

--- campsights_cli/__main__.py
+++ campsights_cli/__main__.py
@@ -21,13 +21,13 @@ def validate_number_of_args(args):
 
 
 def validate_command_arg(command):
-    if command not in ['list', 'get']:
+    if command not in ['camp_list', 'get']:
         raise NameError('\nCommand : {} : not recognized. '
-                        'Please choose from: [ list, get ]\n'.format(command))
+                        'Please choose from: [ camp_list, get ]\n'.format(command))
 
 
 def validate_option_arg(command, option):
-    if command == 'list':
+    if command == 'camp_list':
         if option not in ['-z', '--zipcode', '-a', '--address']:
             raise NameError('\nOption : {opt} : not recognized '
                             'for : {cmd} : command. \nPlease choose from: '
@@ -41,10 +41,37 @@ def validate_option_arg(command, option):
                             '\n'.format(opt=option, cmd=command))
 
 
+def execute_camplist_command(option, user_request):
+    campsights_api = campsights_cli.campsights_api.Client()
+
+    if option in ['-z', '--zipcode']:
+        try:
+            user_request = common.validate_postal_code(user_request)
+            campgrounds = \
+                campsights_api.request_list_of_campgrounds_by_zipcode(
+                    user_request)
+        except TypeError as e:
+            print(str(e))
+            return
+
+    if option in ['-a', '--address']:
+        try:
+            user_request = common.validate_address_format(user_request)
+            campgrounds = \
+                campsights_api.request_list_of_campgrounds_by_address(
+                    user_request)
+        except TypeError as e:
+            print(str(e))
+            return
+
+    display_up_to_ten_campgrounds(campgrounds)
+
+
 def display_up_to_ten_campgrounds(campgrounds):
     if campgrounds is not None and campgrounds:
         total = campgrounds['metadata']['RESULTS']['TOTAL_COUNT']
-        print('Located {} results.\n'.format(total))
+        print('\nLocated {total} results for {address}.\n'.format(
+            total=total, address=campgrounds['address']))
         if total > 10:
             print('Here are the first 10:\n')
         for campground in campgrounds['campgrounds']:
@@ -69,20 +96,8 @@ def main():
         print(str(e))
         return
 
-    campsights_api = campsights_cli.campsights_api.Client()
-    if command == 'list':
-        if option in ['-z', '--zipcode']:
-
-            try:
-                user_request = common.validate_postal_code(user_request)
-                campgrounds = \
-                    campsights_api.request_list_of_campgrounds_by_zipcode(
-                        user_request)
-            except TypeError as e:
-                print(str(e))
-                return
-
-            display_up_to_ten_campgrounds(campgrounds)
+    if command == 'camp_list':
+        execute_camplist_command(option, user_request)
 
 
 if __name__ == '__main__':
